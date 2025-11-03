c185f08ae18d33b2fd4ee29ce62a0b584e04b08a
David19 <davidleonel911@gmail.com>
2025-11-03 17:59:55 -0500

    .

--- djangoCRUD/urls.py
+++ djangoCRUD/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 
-from tareas import views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloWorld),
+    
+    path('', include('tareas.urls')) #el primer parametro es un prefijo para las urls 
 ]
--- tareas/views.py
+++ tareas/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
+from django.contrib.auth.models import User  
+
 # Create your views here.
 
-def helloWorld(request):
-    return render(request, 'singup.html')
+from django.contrib.auth.forms import UserCreationForm 
+
+def signup(request):
+    #return render(request, 'signup.html', {'form':UserCreationForm}) #el nombre form va a contener lo que devuelva el UserCreat...
+    if request.method == 'GET':
+        print("Metodo GET")
+        return render(request, 'signup.html', {'form':UserCreationForm})
+    else:
+        print("Metodo POST")
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1']) #este user el el del orm de django 
+                user.save()#esto lo guarda dentro de la BD
+                return HttpResponse('Usuario guardado con exito')
+            except:
+                return render(request, 'signup.html', {
+                    'form':UserCreationForm,
+                    "error": 'este ususario ya existe'}) 
+        
+        
+        return render(request, 'signup.html', {
+                    'form':UserCreationForm,
+                    "error": 'las contraseñas no coinciden'})
+        
+
+def home(request):
+    return render(request, "home.html")
+
+def hello(request):
+    return render(request, "hello.html")
