3bf51b695c8cdcd5ade76e64708c120c3e92f9c7
David19 <davidleonel911@gmail.com>
2025-11-06 19:43:34 -0500

    .

--- tareas/admin.py
+++ tareas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Tareas #para que la tabla tenga acceso al panel de admimm
+
+
+class taskAdmin(admin.ModelAdmin): #mostrando un "auto_now_add=True" en el panel de admin, antes se ponia solo y no se mostraba
+    readonly_fields = ('fecha_creacion', )
+    
+
 # Register your models here.
+
+#admin.site.register(Tareas) - antes 
+admin.site.register(Tareas, taskAdmin)
--- tareas/models.py
+++ tareas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from django.contrib.auth.models import User #para la FK
 # Create your models here.
+
+
+class Tareas(models.Model):
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True) #si no me pasan nada por defecto va a estar vacio 
+    #fecha en la que fue creada
+    fecha_creacion = models.DateTimeField(auto_now_add=True) # al crearla se agregara la fecha actual si no le pasamos 
+    #fecha en la que la tarea se marco como completada  
+    dia_completada = models.DateTimeField(null=True)#permite valores nulos
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # le indicamos con quien se va a realacionar 
+
+    def __str__(self): #cuando lo usen como un string..
+        return self.titulo + ' por: ' + self.user.username
